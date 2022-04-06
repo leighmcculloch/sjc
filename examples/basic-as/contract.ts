9ae737a56b53658ebccbcc5817392c7848d1a224
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-04-06 09:26:42 -0700

    add test

--- examples/basic-as/contract.ts
+++ examples/basic-as/contract.ts
@@ -1,54 +1,60 @@
 import * as val from "./sdk/val.ts";
 import * as host from "./sdk/host.ts";
 
-const accountsFundedKey = val.fromU32(0x01);
-
-// Init sets up the contract initial state. It must be called once before any
-// other function of the contract is called.
-export function init(): void {
-  if (!val.isVoid(host.store_get(accountsFundedKey))) {
-    return;
-  }
-  host.store_set(accountsFundedKey, host.map_new());
+export function use_all_the_host_functions(): void {
+  host.log_value(0);
+  host.store_set(0, 0);
+  host.store_get(0);
+  host.vec_new();
+  host.vec_put(0, 0, 0);
+  host.vec_get(0, 0);
+  host.vec_del(0, 0);
+  host.vec_len(0);
+  host.vec_push(0, 0);
+  host.vec_pop(0);
+  host.vec_take(0, 0);
+  host.vec_drop(0, 0);
+  host.vec_front(0);
+  host.vec_back(0);
+  host.vec_insert(0, 0, 0);
+  host.vec_append(0, 0);
+  host.map_new();
+  host.map_put(0, 0, 0);
+  host.map_get(0, 0);
+  host.map_del(0, 0);
+  host.map_len(0);
+  host.map_keys(0);
+  host.create_account(0, 0, 0);
+  host.create_trustline(0, 0);
+  host.get_balance(0, 0);
+  host.pay(0, 0, 0, 0);
 }
 
-// Fund accepts an account, creates it with some native asset, and stores
-// it in the list of accounts created by friendbot.
-export function fund(acc: val.Val): val.Val {
-  let accounts = host.store_get(accountsFundedKey);
-  accounts = host.map_put(accounts, acc, val.fromBool(true));
-  host.store_set(accountsFundedKey, accounts);
-  return host.create_account(val.fromVoid(), acc, val.fromU63(1000));
-}
-
-// Was created by friendbot returns if the given account was created by
-// friendbot.
-export function was_created_by_fund(acc: val.Val): val.Val {
-  const accounts = host.store_get(accountsFundedKey);
-  const was = host.map_get(accounts, acc);
-  if (val.isTrue(was)) {
-    return val.fromTrue();
-  }
-  return val.fromFalse();
-}
-
-// Trust asset changes an account so that it trusts an asset, enabling the
-// account to hold the asset.
-export function trust_asset(acc: val.Val, asset: val.Val): val.Val {
-  return host.create_trustline(acc, asset);
-}
-
-// Payment from src to dst of asset.
-export function payment(
-  srcAccId: val.Val,
-  dstAccId: val.Val,
-  asset: val.Val,
-  amount: val.Val,
-): val.Val {
-  return host.pay(srcAccId, dstAccId, asset, amount);
-}
-
-// Get balance gets the balance of an account.
-export function balance(accId: val.Val, asset: val.Val): val.Val {
-  return host.get_balance(accId, asset);
+export function use_all_the_host_functions_2(): void {
+  host.log_value(0);
+  host.store_set(0, 0);
+  host.store_get(0);
+  host.vec_new();
+  host.vec_put(0, 0, 0);
+  host.vec_get(0, 0);
+  host.vec_del(0, 0);
+  host.vec_len(0);
+  host.vec_push(0, 0);
+  host.vec_pop(0);
+  host.vec_take(0, 0);
+  host.vec_drop(0, 0);
+  host.vec_front(0);
+  host.vec_back(0);
+  host.vec_insert(0, 0, 0);
+  host.vec_append(0, 0);
+  host.map_new();
+  host.map_put(0, 0, 0);
+  host.map_get(0, 0);
+  host.map_del(0, 0);
+  host.map_len(0);
+  host.map_keys(0);
+  host.create_account(0, 0, 0);
+  host.create_trustline(0, 0);
+  host.get_balance(0, 0);
+  host.pay(0, 0, 0, 0);
 }
