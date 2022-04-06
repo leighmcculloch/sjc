8ae60cedbb2132546c1284a6d277a7cb9a023f80
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-04-06 09:03:45 -0700

    Reduce host functions

--- examples/basic-as/sdk/host.ts
+++ examples/basic-as/sdk/host.ts
@@ -1,57 +1,66 @@
 import { Val } from "./val.ts";
 
-@external("env", "host__log_value")
-export declare function log_value(v: Val): Val;
-@external("env", "host__store_set")
-export declare function store_set(k: Val, v: Val): void;
-@external("env", "host__store_get")
-export declare function store_get(k: Val): Val;
+const op_log_value: i64 = 100
+const op_store_set: i64 = 200
+const op_store_get: i64 = 300
+const op_vec_new: i64 = 400
+const op_vec_put: i64 = 500
+const op_vec_get: i64 = 600
+const op_vec_del: i64 = 700
+const op_vec_len: i64 = 800
+const op_vec_push: i64 = 900
+const op_vec_pop: i64 = 1000
+const op_vec_take: i64 = 1100
+const op_vec_drop: i64 = 1200
+const op_vec_front: i64 = 1300
+const op_vec_back: i64 = 1400
+const op_vec_insert: i64 = 1500
+const op_vec_append: i64 = 1600
+const op_map_new: i64 = 1700
+const op_map_put: i64 = 1800
+const op_map_get: i64 = 1900
+const op_map_del: i64 = 2000
+const op_map_len: i64 = 2100
+const op_map_keys: i64 = 2200
+const op_create_account: i64 = 2300
+const op_create_trustline: i64 = 2400
+const op_get_balance: i64 = 2500
+const op_pay: i64 = 2600
 
-@external("env", "host__vec_new")
-export declare function vec_new(): Val;
-@external("env", "host__vec_put")
-export declare function vec_put(v: Val, i: Val, x: Val): Val;
-@external("env", "host__vec_get")
-export declare function vec_get(v: Val, i: Val): Val;
-@external("env", "host__vec_del")
-export declare function vec_del(v: Val, i: Val): Val;
-@external("env", "host__vec_len")
-export declare function vec_len(v: Val): Val;
-@external("env", "host__vec_push")
-export declare function vec_push(v: Val, x: Val): Val;
-@external("env", "host__vec_pop")
-export declare function vec_pop(v: Val): Val;
-@external("env", "host__vec_take")
-export declare function vec_take(v: Val, n: Val): Val;
-@external("env", "host__vec_drop")
-export declare function vec_drop(v: Val, n: Val): Val;
-@external("env", "host__vec_front")
-export declare function vec_front(v: Val): Val;
-@external("env", "host__vec_back")
-export declare function vec_back(v: Val): Val;
-@external("env", "host__vec_insert")
-export declare function vec_insert(v: Val, i: Val, x: Val): Val;
-@external("env", "host__vec_append")
-export declare function vec_append(v1: Val, v2: Val): Val;
+@external("env", "h0")
+export declare function h0(op: i64): Val;
+@external("env", "h1")
+export declare function h1(op: i64, a: Val): Val;
+@external("env", "h2")
+export declare function h2(op: i64, a: Val, b: Val): Val;
+@external("env", "h3")
+export declare function h3(op: i64, a: Val, b: Val, c: Val): Val;
+@external("env", "h4")
+export declare function h4(op: i64, a: Val, b: Val, c: Val, d: Val): Val;
 
-@external("env", "host__map_new")
-export declare function map_new(): Val;
-@external("env", "host__map_put")
-export declare function map_put(m: Val, k: Val, v: Val): Val;
-@external("env", "host__map_get")
-export declare function map_get(m: Val, k: Val): Val;
-@external("env", "host__map_del")
-export declare function map_del(m: Val, k: Val): Val;
-@external("env", "host__map_len")
-export declare function map_len(m: Val): Val;
-@external("env", "host__map_keys")
-export declare function map_keys(m: Val): Val;
-
-@external("env", "host__create_account")
-export declare function create_account(srcAcc: Val, dstAcc: Val, startingBalance: Val): Val;
-@external("env", "host__create_trustline")
-export declare function create_trustline(srcAcc: Val, asset: Val): Val;
-@external("env", "host__get_balance")
-export declare function get_balance(acc: Val, asset: Val): Val;
-@external("env", "host__pay")
-export declare function pay(src: Val, dst: Val, asset: Val, amount: Val): Val;
+export function log_value(v: Val): Val { return h1(op_log_value,v) }
+export function store_set(k: Val, v: Val): void { return h2(op_store_set,k,v) }
+export function store_get(k: Val): Val { return h1(op_store_get,k) }
+export function vec_new(): Val { return h0(op_vec_new) }
+export function vec_put(v: Val, i: Val, x: Val): Val { return h3(op_vec_put,v,i,x) }
+export function vec_get(v: Val, i: Val): Val { return h2(op_vec_get,v,i) }
+export function vec_del(v: Val, i: Val): Val { return h2(op_vec_del,v,i) }
+export function vec_len(v: Val): Val { return h1(op_vec_len,v) }
+export function vec_push(v: Val, x: Val): Val { return h2(op_vec_push,v,x) }
+export function vec_pop(v: Val): Val { return h1(op_vec_pop,v) }
+export function vec_take(v: Val, n: Val): Val { return h2(op_vec_take,v,n) }
+export function vec_drop(v: Val, n: Val): Val { return h2(op_vec_drop,v,n) }
+export function vec_front(v: Val): Val { return h1(op_vec_front,v) }
+export function vec_back(v: Val): Val { return h1(op_vec_back,v) }
+export function vec_insert(v: Val, i: Val, x: Val): Val { return h3(op_vec_insert,v,i,x) }
+export function vec_append(v1: Val, v2: Val): Val { return h2(op_vec_append,v1,v2) }
+export function map_new(): Val { return h0(op_map_new) }
+export function map_put(m: Val, k: Val, v: Val): Val { return h3(op_map_put,m,k,v) }
+export function map_get(m: Val, k: Val): Val { return h2(op_map_get,m,k) }
+export function map_del(m: Val, k: Val): Val { return h2(op_map_del,m,k) }
+export function map_len(m: Val): Val { return h1(op_map_len,m) }
+export function map_keys(m: Val): Val { return h1(op_map_keys,m) }
+export function create_account(srcAcc: Val, dstAcc: Val, startingBalance: Val): Val { return h3(op_create_account,srcAcc,dstAcc,startingBalance) }
+export function create_trustline(srcAcc: Val, asset: Val): Val { return h2(op_create_trustline,srcAcc,asset) }
+export function get_balance(acc: Val, asset: Val): Val { return h2(op_get_balance,acc,asset) }
+export function pay(src: Val, dst: Val, asset: Val, amount: Val): Val { return h4(op_pay,src,dst,asset,amount) }
